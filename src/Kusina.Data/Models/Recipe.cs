1765af16961a0eb4e95cf2a80e135c4371116b32
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2019-03-03 18:00:44 -0800

    Add solution and initial project
    Added Kusina.Data project as .NET Standard. This will contain all
    EntityFramework-related models.

---
+++ src/Kusina.Data/Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kusina.Data.Models
+{
+    public class Recipe
+    {
+        public Guid RecipeId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public ICollection<Ingredient> Ingredients { get; set; }
+        public ICollection<Step> Steps { get; set; }
+    }
+}

10bf0b796ff179bf36e4bd191fac18d89c70d883
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2019-05-13 20:40:02 -0700

    Add infrastructure layer

---
+++ src/Kusina.Infrastructure/MachineDateTime.cs
@@ -0,0 +1,14 @@
+﻿using Kusina.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kusina.Infrastructure
+{
+    public class MachineDateTime : IDateTime
+    {
+        public DateTime Now => DateTime.Now;
+
+        public int CurrentYear => DateTime.Now.Year;
+    }
+}

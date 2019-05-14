3d4ff72f6f268f11674f66b6f066053f72449333
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2019-05-13 20:00:41 -0700

    Add application layer

---
+++ src/Kusina.Application/Interfaces/IKusinaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Kusina.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kusina.Application.Interfaces
+{
+    public interface IKusinaDbContext
+    {
+        DbSet<Category> Categories { get; set; }
+        DbSet<Ingredient> Ingredients { get; set; }
+        DbSet<Recipe> Recipes { get; set; }
+        DbSet<Step> Steps { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}

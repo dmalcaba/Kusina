8977714ec506a02bbe9411438d46446efa132549
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2019-05-13 19:35:33 -0700

    Add persistence project

---
+++ src/Kusina.Persistence/KusinaDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Kusina.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kusina.Persistence
+{
+    public class KusinaDbContext : DbContext
+    {
+        public KusinaDbContext()
+        {
+        }
+
+        public KusinaDbContext(DbContextOptions<KusinaDbContext> options)
+            : base (options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Ingredient> Ingredients { get; set; }
+        public virtual DbSet<Recipe> Recipes { get; set; }
+        public virtual DbSet<Step> Steps { get; set; }
+    }
+}
